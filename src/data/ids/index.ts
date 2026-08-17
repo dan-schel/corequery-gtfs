1bc8589f46f912de8bbca9421455b26b4d188452
Dan Schellekens <me@danschellekens.com>
2026-08-17 18:00:08 +1000

    even more exports (#11)

--- src/data/ids/index.ts
+++ src/data/ids/index.ts
@@ -1,2 +1,6 @@
+export * from "./line-gtfs-id-collection.js";
 export * from "./line-gtfs-id-mapping.js";
+export * from "./line-gtfs-id-metadata.js";
+export * from "./stop-gtfs-id-collection.js";
 export * from "./stop-gtfs-id-mapping.js";
+export * from "./stop-gtfs-id-metadata.js";
--- src/data/index.ts
+++ src/data/index.ts
@@ -2,3 +2,7 @@ export * from "./ids/index.js";
 export * from "./route/index.js";
 
 export * from "./gtfs-stop-time.js";
+export * from "./gtfs-scheduled-trip.js";
+export * from "./gtfs-scheduled-trip-movements.js";
+export * from "./gtfs-updated-trip.js";
+export * from "./gtfs-updated-trip-movements.js";
