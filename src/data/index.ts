20b6fd82ea80fcf93f4be6e6b5dae9b7aff93c61
dan-schel <me@danschellekens.com>
2026-08-30 17:49:25 +1000

    remove unused exports

--- src/data/index.ts
+++ src/data/index.ts
@@ -3,8 +3,3 @@ export * from "./route/index.js";
 
 export * from "./gtfs-feed.js";
 export * from "./gtfs-stop-time.js";
-export * from "./gtfs-scheduled-trip.js";
-export * from "./gtfs-scheduled-trip-movements.js";
-export * from "./gtfs-updated-trip.js";
-export * from "./gtfs-updated-trip-movements.js";
-export * from "./utils.js";
