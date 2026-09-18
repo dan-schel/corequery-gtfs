9edbd91d1a1d08bd9d6421db421786a97d9f6f51
dan-schel <me@danschellekens.com>
2026-09-18 21:10:53 +1000

    unexport

--- src/corequery-types.ts
+++ src/corequery-types.ts
@@ -121,7 +121,7 @@ export type Color =
 
 type ServiceLiveDataType = "scheduled" | "updated" | "added";
 
-export type ServiceTimeType =
+type ServiceTimeType =
   "scheduled-time" | "provided-live-time" | "interpolated-live-time";
 
 type ServiceConnectionType = "entire-vehicle-forms-service" | "other";
